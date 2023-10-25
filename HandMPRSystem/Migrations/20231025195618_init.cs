da4abaa29edcc914cfdee4270c80d9cfe80eb173
MegMoore <136514941+MegMoore@users.noreply.github.com>
2023-10-25 16:23:21 -0400

    Add project files.

---
+++ HandMPRSystem/Migrations/20231025195618_init.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HandMPRSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Firstname = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Lastname = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    City = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    State = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    Zip = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    IsReviewer = table.Column<bool>(type: "bit", nullable: false),
+                    IsAdmin = table.Column<bool>(type: "bit", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
---
+++ HandMPRSystem/Models/Client.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HandMPRSystem.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        [StringLength(30)]
+        public string Firstname { get; set; } = string.Empty;
+        [StringLength(30)]
+        public string Lastname { get; set; } = string.Empty;
+        [StringLength(255)]
+        public string? Email { get; set; } = string.Empty;
+        [StringLength(12)]
+        public string? Phone { get; set; } = string.Empty;
+        [StringLength(80)]
+        public string Address { get; set; } = string.Empty;
+        [StringLength(30)]
+        public string City { get; set; } = string.Empty;
+        [StringLength(2)]
+        public string State { get; set; } = string.Empty;
+        [StringLength(5)]
+        public string Zip { get; set; } = string.Empty;
+        public bool IsReviewer { get; set; } = false;
+        public bool IsAdmin { get; set; } = false;
+        [StringLength(30)]
+        public string Username { get; set; } = string.Empty;
+        [StringLength(30)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
