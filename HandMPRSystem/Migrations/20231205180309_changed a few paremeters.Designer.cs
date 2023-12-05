9051ab0af654b81afc488a1b7c3237a82c6a528e
MegMoore <136514941+MegMoore@users.noreply.github.com>
2023-12-05 13:03:42 -0500

    updated parameters

--- HandMPRSystem/Migrations/20231205180309_changed a few paremeters.Designer.cs
+++ HandMPRSystem/Migrations/20231205180309_changed a few paremeters.Designer.cs
@@ -4,6 +4,7 @@ using HandMPRSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HandMPRSystem.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231205180309_changed a few paremeters")]
+    partial class changedafewparemeters
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HandMPRSystem/Migrations/20231205180309_changed a few paremeters.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HandMPRSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedafewparemeters : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Quilt",
+                table: "Quilt");
+
+            migrationBuilder.RenameTable(
+                name: "Quilt",
+                newName: "Quilts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Quilts",
+                table: "Quilts",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Quilts",
+                table: "Quilts");
+
+            migrationBuilder.RenameTable(
+                name: "Quilts",
+                newName: "Quilt");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Quilt",
+                table: "Quilt",
+                column: "Id");
+        }
+    }
+}
